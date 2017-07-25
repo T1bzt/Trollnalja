0ac91389d2a80db2a3db65fbe5c5bd821a59f7e3
vanekcsi <p.marton95@gmail.com>
2017-07-25 16:30:31 +0200

    bootrstrap trolnalja
    sklendondzonsz jqueryy

---
+++ TrollnaljaBootstrap/js/index.js
@@ -0,0 +1,20 @@
+$(function () {
+
+    /*$( "#zaszloSk" ).click(function() {
+        $(".hu").attr('hidden', true);
+        $(".sk").attr('hidden', false);
+    });
+
+    $( "#zaszloHu" ).click(function () {
+        $(".hu").attr('hidden', false);
+        $(".sk").attr('hidden', true);
+    });*/
+
+    /*$( "#zaszloSk" ).click(function() {
+        $(".sk").show();
+        $(".hu").hide();
+    });*/
+
+
+});
+
